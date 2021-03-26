bc243ae4a72922729b84be633379b0e31735bcce
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-03-26 07:22:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ for each_link in range(3, 22):
 		pass
 	GSEARCH.scrape(response, google_search_urls, each_link, index_num)
 	GSEARCH.sleep()
-GSEARCH.create_dataframe()
+df = GSEARCH.create_dataframe()
 GSEARCH.export_to_csv(df)
 
