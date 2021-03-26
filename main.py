960816cdf71d4ed742931b5653d81692eeac7032
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-03-26 06:53:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,12 @@ for each_link in range(3, 22):
 	index_num = google_search_urls.index(google_search_urls[each_link])
 	link = google_search_urls[each_link]
 	response = GSEARCH.set_UA_headers_and_send_request(link, session)
-	error_429_handle(response)
+	if response.status_code = 429:
+		response = GSEARCH.error_429_handle()
+	else:
+		pass
 	GSEARCH.scrape(response, google_search_urls, each_link, index_num)
 	GSEARCH.sleep()
 GSEARCH.create_dataframe()
 GSEARCH.export_to_csv(df)
 
-def error_429_handle(response):
-	if response.status_code = 429:
-		time.sleep(120)
-		session.cookies.clear()
-		session.exit()
-		session = HTMLSession()
-	else:
-		pass
-	return session
