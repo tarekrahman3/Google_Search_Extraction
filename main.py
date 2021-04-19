4ff50b734495357ca34d1c0194a10993fde5f611
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-04-19 20:08:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import GSEARCH
-
-google_search_urls = GSEARCH.load_import_csv()
+google_search_urls = GSEARCH.dict_csv_read()
 session = GSEARCH.start_request_session()
-
-for each_link in range(3, 22):
-	index_num = google_search_urls.index(google_search_urls[each_link])
-	link = google_search_urls[each_link]
-	response = GSEARCH.set_UA_headers_and_send_request(link, session)
-	if response.status_code == 429:
-		response = GSEARCH.error_429_handle()
-	else:
-		pass
-	GSEARCH.test_no_result(response)
-	GSEARCH.scrape(response, google_search_urls, each_link, index_num)
-	GSEARCH.sleep()
-df = GSEARCH.create_dataframe()
-GSEARCH.export_to_csv(df)
-
+dict_array = []
+sleep_time = int(input('interval period (in second): '))
+try:
+	for i in google_search_urls:
+		index_num = google_search_urls.index(i)
+		link = i
+		response = GSEARCH.set_UA_headers_and_send_request(i, session)
+		if response.status_code == 429:
+			response = GSEARCH.error_429_handle(session)
+		else:
+			pass
+		info = GSEARCH.scrape_all_result(response, google_search_urls, i, index_num)
+		#info = GSEARCH.scrape_first_three_result(response, google_search_urls, each_link, index_num)
+		dict_array.append(info)
+		GSEARCH.sleep(sleep_time)
+finally:
+	GSEARCH.write_csv(dict_array)
