843c55b234d00575306af704460f8975da485f71
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-10-18 21:37:14 +0600

    Update Google_Search_Scraper.py

--- Google_Search_Scraper.py
+++ Google_Search_Scraper.py
@@ -15,28 +15,28 @@ def dict_csv_read():
 def scrape(url, index, dict_array):
 	driver.get(url+'&hl=en')
 	time.sleep(2)
-	if "captcha" in (driver.page_source):
+	if "robot" in (driver.page_source):
 		play_obj = wave_obj.play()
 		play_obj.wait_done()
 		input("Press any key to continue")
 	try:
-		permanently_closed = driver.find_element_by_xpath('//h2[text()="Complementary results"]/following-sibling::div//span[text()="Permanently closed"]').text
+		permanently_closed = driver.find_element_by_xpath('//h2[text()="Complementary results" or text()="Complementary Results"]/following-sibling::div//span[text()="Permanently closed"]').text
 	except:
 		permanently_closed = None
 	try:
-		name = driver.find_element_by_xpath('//h2[text()="Complementary results"]/following-sibling::div//h2/span').text
+		name = driver.find_element_by_xpath('//h2[text()="Complementary results" or text()="Complementary Results"]/following-sibling::div//h2/span').text
 	except:
 		name = None
 	try:
-		address = driver.find_element_by_xpath('//h2[text()="Complementary results"]/following-sibling::div//a[text()="Address"]/../following-sibling::span').text
+		address = driver.find_element_by_xpath('//h2[text()="Complementary results" or text()="Complementary Results"]/following-sibling::div//a[text()="Address"]/../following-sibling::span').text
 	except:
 		address = None
 	try:
-		website = driver.find_element_by_xpath('//h2[text()="Complementary results"]/following-sibling::div//a/div[text()="Website"]/..').get_attribute('href')
+		website = driver.find_element_by_xpath('//h2[text()="Complementary results" or text()="Complementary Results"]/following-sibling::div//a/div[text()="Website"]/..').get_attribute('href')
 	except:
 		website = None
 	try:
-		phone =  driver.find_element_by_xpath('//h2[text()="Complementary results"]/following-sibling::div//span[contains(@aria-label,"Call")]').text
+		phone =  driver.find_element_by_xpath('//h2[text()="Complementary results" or text()="Complementary Results"]/following-sibling::div//span[contains(@aria-label,"Call")]').text
 	except:
 		phone =  None
 	try:
@@ -51,17 +51,43 @@ def scrape(url, index, dict_array):
 		first_result_url = driver.find_element_by_xpath('//h3/ancestor::a').get_attribute('href')
 	except:
 		first_result_url = None
+	try:
+		social_profiles =  driver.find_element_by_xpath('//h2[text()="Complementary results" or text()="Complementary Results"]/following-sibling::div//*[text()="Profiles"]/following::div[1]')
+		try:
+			facebook = social_profiles.find_element_by_xpath('.//a[contains(@href,"facebook.com/")]').get_attribute('href')
+		except:
+			facebook = None
+		try:
+			instagram = social_profiles.find_element_by_xpath('.//a[contains(@href,"instagram.com/")]').get_attribute('href')
+		except:
+			instagram = None
+		try:
+			twitter = social_profiles.find_element_by_xpath('.//a[contains(@href,"twitter.com/")]').get_attribute('href')
+		except:
+			twitter = None
+		try:
+			linkedin = social_profiles.find_element_by_xpath('.//a[contains(@href,"linkedin.com/")]').get_attribute('href')
+		except:
+			linkedin = None
+		socials = f"""
+			facebook: {facebook}
+			instagram: {instagram}
+			twitter: {twitter}
+			linkedin: {linkedin}"""
+	except:
+		socials = None
 	data = {'source':url,
 	'name':name,
 	'address':address,
 	'website':website,
 	'phone':phone,
 	'permanently_closed':permanently_closed,
+	'socials':socials,
 	'1st_result_title': title,
 	'1st_result_description': description,
 	'1st_result_url': first_result_url
 	}
-	print(f"{index} | {data['name']} | {data['website']} | {data['phone']}")
+	print(f"{index} | {data['name']} | {data['website']} | {data['phone']} | {data['socials']}")
 	dict_array.append(data)
 
 google_search_urls = dict_csv_read()
